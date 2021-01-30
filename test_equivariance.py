4fcd90a22823b42ae2a7af92bd894ad640bc376d
Phil Wang <lucidrains@gmail.com>
2021-01-29 23:35:23 -0800

    add another equivariance test

--- test_equivariance.py
+++ test_equivariance.py
@@ -35,3 +35,26 @@ def test_equivariance():
 
     diff = (out1 - out2).max()
     assert diff < 1e-4, 'is not equivariant'
+
+def test_equivariance_with_type_one_input():
+    model = SE3Transformer(
+        dim = 64,
+        depth = 2,
+        attend_self = True,
+        num_degrees = 2,
+        input_degrees = 2,
+        output_degrees = 2
+    )
+
+    atom_features = torch.randn(1, 32, 64, 1)
+    pred_coors = torch.randn(1, 32, 64, 3)
+
+    coors = torch.randn(1, 32, 3)
+    mask  = torch.ones(1, 32).bool()
+
+    R   = rot(15, 0, 45)
+    out1 = model({'0': atom_features, '1': pred_coors @ R}, coors @ R, mask, return_type = 1)
+    out2 = model({'0': atom_features, '1': pred_coors}, coors, mask, return_type = 1) @ R
+
+    diff = (out1 - out2).max()
+    assert diff < 1e-4, 'is not equivariant'

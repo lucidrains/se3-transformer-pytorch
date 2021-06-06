b582975ccd904f18431018c530c2fbe16d6d5d40
Phil Wang <lucidrains@gmail.com>
2021-06-06 10:54:55 -0700

    add ability to learn scaling within NormSE3 - removed the learned bias due to it potentially breaking equivariance

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'se3-transformer-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.7.0',
+  version = '0.7.3',
   license='MIT',
   description = 'SE3 Transformer - Pytorch',
   author = 'Phil Wang',

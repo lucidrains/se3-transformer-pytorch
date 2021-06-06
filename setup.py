470d4d46ef6b1bdc6f80b02da03aceeddd92eb22
Phil Wang <lucidrains@gmail.com>
2021-06-06 15:44:22 -0700

    0.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'se3-transformer-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.8.0',
+  version = '0.8.1',
   license='MIT',
   description = 'SE3 Transformer - Pytorch',
   author = 'Phil Wang',

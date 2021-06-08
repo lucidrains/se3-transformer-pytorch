1fe1615de7010d324abc0a42dffa5c69da8fc2f2
Phil Wang <lucidrains@gmail.com>
2021-06-08 11:30:19 -0700

    vector gating release for tcn -> egnn

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'se3-transformer-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.8.6',
+  version = '0.8.7',
   license='MIT',
   description = 'SE3 Transformer - Pytorch',
   author = 'Phil Wang',
